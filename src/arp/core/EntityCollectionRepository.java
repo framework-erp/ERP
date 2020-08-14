ca1be8ba88ad20febfdeda7e6b8243f8caeebd8d
zcd <zcd@wooduan2019>
2020-08-14 21:49:36 +0800

    继续

--- src/arp/core/EntityCollectionRepository.java
+++ src/arp/core/EntityCollectionRepository.java
@@ -1,5 +1,6 @@
 package arp.core;
 
+import java.util.Map;
 import java.util.concurrent.atomic.AtomicInteger;
 
 public abstract class EntityCollectionRepository<ID, T> {
@@ -36,4 +37,103 @@ public abstract class EntityCollectionRepository<ID, T> {
 		}
 		return entity;
 	}
+
+	public T copy(ID id) {
+		ProcessContext processContext = ThreadBoundProcessContextArray.getProcessContext();
+		if (!processContext.isStarted()) {
+			throw new RuntimeException("can not use repository without a process");
+		}
+
+		T entityInProcess = processContext.findCollectionEntityInProcessForRead(this.id, id);
+		if (entityInProcess != null) {
+			return entityInProcess;
+		}
+
+		if (processContext.collectionEntityRemovedInProcess(this.id, id)) {
+			return null;
+		}
+
+		T entity = store.findByIdForRead(id);
+		if (entity != null) {
+			processContext.putCollectionEntityInProcessForGetForRead(this.id, id, entity);
+			processContext.addEntityCollectionRepository(this.id, this);
+		}
+		return entity;
+	}
+
+	public T put(T entity) {
+
+		ProcessContext processContext = ThreadBoundProcessContextArray.getProcessContext();
+		if (!processContext.isStarted()) {
+			throw new RuntimeException("can not use repository without a process");
+		}
+
+		ID id = getId(entity);
+		if (processContext.collectionEntityRemovedInProcess(this.id, id)) {
+			T entityInProcess = processContext.findCollectionEntityInProcess(this.id, id);
+			if (entityInProcess != null) {
+				return entityInProcess;
+			}
+			processContext.putCollectionEntityInProcessForPut(this.id, id, entity);
+			processContext.addEntityCollectionRepository(this.id, this);
+			return entity;
+		}
+
+		acquireLock(id, processContext);
+
+		T entityInProcess = processContext.findCollectionEntityInProcess(this.id, id);
+		if (entityInProcess != null) {
+			return entityInProcess;
+		}
+
+		if (store.has(id)) {
+			T existsEntity = store.findByIdForUpdate(id);
+			processContext.putCollectionEntityInProcessForGet(this.id, id, existsEntity);
+			processContext.addEntityCollectionRepository(this.id, this);
+			return existsEntity;
+		}
+
+		processContext.putCollectionEntityInProcessForPut(this.id, id, entity);
+		processContext.addEntityCollectionRepository(this.id, this);
+		return entity;
+	}
+
+	public T putIfAbsent(T entity) {
+
+	}
+
+	protected abstract ID getId(T entity);
+
+	void updateEntitiesInProcess(Map<ID, T> entities) {
+		store.checkAndUpdateAll(entities);
+	}
+
+	void saveEntitiesInProcess(Map<ID, T> entities) {
+		store.saveAll(entities);
+	}
+
+	void removeEntitiesInProcess(Map<ID, T> entities) {
+		store.removeAll(entities.keySet());
+	}
+
+	public T remove(ID id) {
+		ProcessContext processContext = ThreadBoundProcessContextArray.getProcessContext();
+		T entityInProcess = processContext.findCollectionEntityInProcess(this.id, id);
+		if (entityInProcess != null) {
+			processContext.removeCollectionEntityInProcess(this.id, id);
+			return entityInProcess;
+		}
+
+		if (processContext.collectionEntityRemovedInProcess(this.id, id)) {
+			return null;
+		}
+
+		acquireLock(id, processContext);
+
+		T existsEntity = store.findByIdForRemoveReturn(id);
+		processContext.putCollectionEntityInProcessForRemove(this.id, id, existsEntity);
+		return existsEntity;
+
+	}
+
 }
