30d44b19d50ed8e50151b83afc99336f87151445
zcd <zcd@wooduan2019>
2020-08-15 20:35:30 +0800

    继续

---
+++ src/arp/core/ProcessWrapper.java
@@ -0,0 +1,20 @@
+package arp.core;
+
+public class ProcessWrapper {
+
+	public static void beforeProcessStart() {
+		ProcessContext processContext = ThreadBoundProcessContextArray.getProcessContext();
+		processContext.startProcess();
+	}
+
+	public static void afterProcessFinish() {
+		ProcessContext processContext = ThreadBoundProcessContextArray.getProcessContext();
+		processContext.finishProcess();
+	}
+
+	public static void afterProcessFaild() {
+		ProcessContext processContext = ThreadBoundProcessContextArray.getProcessContext();
+		processContext.processFaild();
+	}
+
+}
