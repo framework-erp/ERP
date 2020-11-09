22e5e89b38cac0e726751f2759e923a81621f285
DELL <DELL@DESKTOP-IUTQH0F>
2020-11-10 00:12:16 +0800

    继续

--- src/arp/core/MessageProcessor.java
+++ src/arp/core/MessageProcessor.java
@@ -1,31 +1,64 @@
 package arp.core;
 
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
 public class MessageProcessor {
 
-	private static Object[] listenerProcessObjects;// listener处理对象
-	private static int listenerProcessObjectsIdx;
+	// listener处理对象
+	private static Object[] listenerProcessObjects = new Object[2];
 
-	private static Map<String, List<Integer>> processListenersIndex;// 过程的listeners索引
+	// 过程的listeners索引
+	private static Map<String, List<Integer>> processListenersIndex = new HashMap<>();
 
 	public static void defineListener(int idx, String processDesc) {
-
+		List<Integer> list = processListenersIndex.get(processDesc);
+		if (list == null) {
+			list = new ArrayList<>();
+			processListenersIndex.put(processDesc, list);
+		}
+		list.add(idx);
 	}
 
-	public static void addListener(int idx, String processDesc) {
+	public static void addListener(int idx, Object listener) {
+		if (idx == (listenerProcessObjects.length - 1)) {
+			resizeListenerProcessObjects(idx);
+		}
+		listenerProcessObjects[idx] = listener;
+	}
 
+	private static void resizeListenerProcessObjects(int idx) {
+		Object[] old = listenerProcessObjects;
+		listenerProcessObjects = new Object[listenerProcessObjects.length * 2];
+		System.arraycopy(old, 0, listenerProcessObjects, 0, idx + 1);
 	}
 
 	public void process(Message msg) {
 		String processDesc = msg.getProcessDesc();
-		// TODO 字节码生成开始
-		if (processDesc.equals("aaa")) {
-			// TODO 注意，对于"aaa"在一个应用内可能有多个listener,要挨个执行
+		List<Integer> listenersIndex = processListenersIndex.get(processDesc);
+		if (listenersIndex == null) {
 			return;
 		}
-		if (processDesc.equals("bbb")) {
+		for (int listenerIndex : listenersIndex) {
+			processByListener(listenerIndex, listenerProcessObjects[listenerIndex], msg);
+		}
+	}
+
+	private void processByListener(int listenerIndex, Object listenerProcessObject, Message msg) {
+		// TODO 字节码生成开始
+		if (listenerIndex == 0) {
+//			service1 = listenerProcessObject;
+//			 service1.receiveAAA()
+			return;
+		}
+		if (listenerIndex == 1) {
+			// service2.receiveAAA()
+			return;
+		}
+		if (listenerIndex == 2) {
+			// service2.receiveBBB()
 			return;
 		}
 		// 字节码生成结束
