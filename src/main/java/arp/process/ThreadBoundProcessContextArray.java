6e672e72bdb5a0a398d257885c6d48838bc4d95c
DELL <DELL@DESKTOP-IUTQH0F>
2021-08-27 11:42:58 +0800

    resize threadProcessContextArray

--- src/main/java/arp/process/ThreadBoundProcessContextArray.java
+++ src/main/java/arp/process/ThreadBoundProcessContextArray.java
@@ -1,18 +1,30 @@
 package arp.process;
 
-import arp.util.Unsafe;
 
 public class ThreadBoundProcessContextArray {
-	private static ProcessContext[] threadProcessContextArray = new ProcessContext[1024
-			+ (Unsafe.objArray_BUFFER_PAD * 2)];// 头尾要处理伪共享
+	private static ProcessContext[] threadProcessContextArray = new ProcessContext[1024];
 
 	public static ProcessContext getProcessContext() {
 		int iTid = (int) Thread.currentThread().getId();
-		ProcessContext ctx = threadProcessContextArray[iTid + Unsafe.objArray_BUFFER_PAD];
+		if (iTid >= threadProcessContextArray.length) {
+			resizeArray(iTid);
+		}
+		ProcessContext ctx = threadProcessContextArray[iTid];
 		if (ctx == null) {
 			ctx = new ProcessContext();
-			threadProcessContextArray[iTid + Unsafe.objArray_BUFFER_PAD] = ctx;
+			threadProcessContextArray[iTid] = ctx;
 		}
 		return ctx;
 	}
+
+	private static synchronized void resizeArray(int iTid) {
+		if (iTid < threadProcessContextArray.length) {
+			return;
+		}
+		ProcessContext[] newArray = new ProcessContext[iTid * 2];
+		System.arraycopy(threadProcessContextArray, 0, newArray, 0,
+				threadProcessContextArray.length);
+		threadProcessContextArray = newArray;
+	}
+
 }
