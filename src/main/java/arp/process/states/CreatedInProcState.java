8db5532ec547fc3db44962b9a4e095626306920b
neo <13758242402@163.com>
2022-11-14 22:52:10 +0800

    重写过程中的实体状态模型

---
+++ src/main/java/arp/process/states/CreatedInProcState.java
@@ -0,0 +1,36 @@
+package arp.process.states;
+
+/**
+ * 在过程中新建的状态
+ */
+public class CreatedInProcState implements ProcessEntityState{
+    @Override
+    public ProcessEntityState transferByTake() {
+        return this;
+    }
+
+    @Override
+    public ProcessEntityState transferByPut() {
+        return new ErrorState();
+    }
+
+    @Override
+    public ProcessEntityState transferByPutIfAbsent() {
+        return this;
+    }
+
+    @Override
+    public ProcessEntityState transferByRemove() {
+        return new TransientInProcState();
+    }
+
+    @Override
+    public boolean isEntityAvailable() {
+        return true;
+    }
+
+    @Override
+    public boolean isAddByTake() {
+        return false;
+    }
+}
---
+++ src/main/java/arp/process/states/ToRemoveInRepoState.java
@@ -0,0 +1,36 @@
+package arp.process.states;
+
+/**
+ * 需要去仓库中删除的状态
+ */
+public class ToRemoveInRepoState implements ProcessEntityState{
+    @Override
+    public ProcessEntityState transferByTake() {
+        return new ErrorState();
+    }
+
+    @Override
+    public ProcessEntityState transferByPut() {
+        return new TakenFromRepoState();
+    }
+
+    @Override
+    public ProcessEntityState transferByPutIfAbsent() {
+        return new TakenFromRepoState();
+    }
+
+    @Override
+    public ProcessEntityState transferByRemove() {
+        return this;
+    }
+
+    @Override
+    public boolean isEntityAvailable() {
+        return false;
+    }
+
+    @Override
+    public boolean isAddByTake() {
+        return true;
+    }
+}
